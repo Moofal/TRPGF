b35ebd880aeaf1cec0e319450581bb802759daa0
anton <antonk@hiof.no>
2022-02-16 09:47:12 +0100

    Changes by Robin Dahlman

--- src/main/java/com/example/example/HelloApplication.java
+++ src/main/java/com/example/example/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.example;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,6 +19,15 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+        Choice choiceBox = new Choice();
+        choiceBox.createChoices(1, "Would u like a room", Color.GREEN);
+        choiceBox.addOption(1, 1, "Innkeeper blah blah", Color.GREEN, 3);
+        choiceBox.addOption(2, 1, "yes 50 please", Color.GREEN, 4);
+        choiceBox.addOptionStatCheck(3, 1, "let me drink 100 beers", Color.GREEN, 200, "Strength", 15);
+        choiceBox.createChoices(20, "There is a dragon in front of you what do you do", Color.GREEN);
+        choiceBox.addOption(522, 20, "Innkeeper blah blah", Color.GREEN, 3);
+        choiceBox.addOption(5425, 20, "seduce the dragon", Color.GREEN, 0);
+
         launch();
     }
 }
--- src/main/java/com/example/example/Scene.java
+++ src/main/java/com/example/example/Scene.java
@@ -1,4 +1,14 @@
 package com.example.example;
 
+import javafx.scene.paint.Color;
+
 public class Scene {
+
+    Scene () {
+
+    }
+
+    public void create(int id, String sceneText, Color textColor, boolean startOver, boolean exit) {
+
+    }
 }

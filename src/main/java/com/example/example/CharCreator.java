631e9319d5239d56195e9368c2f376ef3fb108ff
anton <antonk@hiof.no>
2022-02-16 09:47:06 +0100

    first

--- src/main/java/com/example/example/CharCreator.java
+++ src/main/java/com/example/example/CharCreator.java
@@ -1,4 +1,20 @@
 package com.example.example;
 
 public class CharCreator {
+
+    CharCreator() {
+
+    }
+
+    public void create(String name) {
+
+    }
+
+    public void addStat(String name, int minValue, int maxValue, int numberOfDice, int diceType) {
+
+    }
+
+    public void addAttribute(String name, int minValue, int maxValue, int startingValue) {
+
+    }
 }
